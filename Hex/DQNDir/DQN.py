aa89ee2a8f3353f55f3d5960e8bc962e21f31aea
lolOzzi <stillingoscar@gmail.com>
2025-06-11 15:21:41 +0200

    Update DQN.py

--- Hex/DQNDir/DQN.py
+++ Hex/DQNDir/DQN.py
@@ -139,10 +139,17 @@ class DQNAgent:
             future_q_values = self.target_model(next_states, training=False)
             max_future_q = tf.reduce_max(future_q_values, axis=1)
             
-            # Corrected line
+            # The calculation of target_q is done in float32 for better precision
             target_q = rewards + (1 - tf.cast(dones, tf.float32)) * DISCOUNT * tf.cast(max_future_q, tf.float32)
+            
             mask = tf.one_hot(tf.cast(actions, dtype=tf.int32), self.env.ACTION_SPACE_SIZE)
             q_values = self.model(states, training=True)
+            
+            # --- FIX STARTS HERE ---
+            # Cast the mask to the same dtype as q_values to prevent the type mismatch
+            mask = tf.cast(mask, q_values.dtype)
+            # --- FIX ENDS HERE ---
+
             predicted_q = tf.reduce_sum(tf.multiply(q_values, mask), axis=1)
             
             loss = tf.keras.losses.MSE(target_q, predicted_q)
