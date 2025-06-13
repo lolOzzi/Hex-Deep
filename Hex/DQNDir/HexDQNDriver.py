4c989c19af902da06f3913d4a955048f89aa6f97
lolOzzi <stillingoscar@gmail.com>
2025-06-13 15:42:28 +0200

    Update HexDQNDriver.py

--- Hex/DQNDir/HexDQNDriver.py
+++ Hex/DQNDir/HexDQNDriver.py
@@ -13,8 +13,8 @@ env = HexEnv()
 agent = DQNAgent(env)
 
 
-#model_file = 'models/5x5-Hybrid-GNN-ConvNet-SwapFlag_____5.00max____3.80avg____0.00min__1749681670.keras'
-model_file = None
+model_file = 'models/5x5-Hybrid_____5.00max____2.60avg____0.00min__1749817832.keras'
+#model_file = None
 
 
 def loadModel(model_file):
