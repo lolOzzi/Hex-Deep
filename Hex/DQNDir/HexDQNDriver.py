7fb349c2dd25d62172585f48ed605b46a6d898f2
lolOzzi <stillingoscar@gmail.com>
2025-06-14 21:12:26 +0200

    aa

--- Hex/DQNDir/HexDQNDriver.py
+++ Hex/DQNDir/HexDQNDriver.py
@@ -13,7 +13,7 @@ env = HexEnv()
 agent = DQNAgent(env)
 
 
-model_file = 'models/5x5-Hybrid-PER-Optimized_____5.00max____3.00avg____0.00min__1749830549.keras'
+model_file = 'models/5x5-Hybrid-PER-Optimized_____5.00max____3.05avg____0.00min__1749917883.keras'
 #model_file = None
 
 
